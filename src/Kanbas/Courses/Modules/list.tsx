1e79b12cb3dc789e296d42004165a39145cc8f7b
Maggie Collette <collette.m@northeastern.edu>
2024-03-17 09:58:04 -0400

    modules working with redux

--- src/Kanbas/Courses/Modules/list.tsx
+++ src/Kanbas/Courses/Modules/list.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
-import { modules } from "../../Database";
+import { useState } from "react";
 import {
 	FaEllipsisV,
 	FaCheckCircle,
 	FaPlusCircle,
 	FaPlus,
+	FaRegTrashAlt,
 } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import "./index.css";
+import { FaRegPenToSquare } from "react-icons/fa6";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, deleteModule, updateModule, setModule } from "./reducer";
+import { KanbasState } from "../../store";
 function ModuleList() {
 	const { courseId } = useParams();
-	const modulesList = modules.filter((module) => module.course === courseId);
-	const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+	const moduleList = useSelector(
+		(state: KanbasState) => state.modulesReducer.modules
+	);
+	const module = useSelector(
+		(state: KanbasState) => state.modulesReducer.module
+	);
+	const dispatch = useDispatch();
+	const [selectedModule, setSelectedModule] = useState(moduleList[0]);
+	const [showNewModule, setShowNewModule] = useState(false);
+	const [showModuleEdit, setShowModuleEdit] = useState(false);
+
 	return (
 		<div className="flex-fill">
 			<span className="d-flex justify-content-end wd-home-buttons">
@@ -39,7 +52,10 @@ function ModuleList() {
 						</li>
 					</ul>
 				</div>
-				<button type="button" className="btn btn-danger text-light">
+				<button
+					type="button"
+					className="btn btn-danger text-light"
+					onClick={() => setShowNewModule(true)}>
 					<FaPlus style={{ padding: 1 }} />
 					Module
 				</button>
@@ -49,37 +65,131 @@ function ModuleList() {
 			</span>
 			<hr />
 			<ul className="list-group wd-modules">
-				{modulesList.map((module, index) => (
-					<li
-						key={index}
-						className="list-group-item"
-						onClick={() => setSelectedModule(module)}>
-						<div>
-							<FaEllipsisV className="me-2" />
-							{module.name}
-							<span className="float-end">
-								<FaCheckCircle className="text-success" />
-								<FaPlusCircle className="ms-2" />
-								<FaEllipsisV className="ms-2" />
-							</span>
-						</div>
-						{selectedModule._id === module._id && (
-							<ul className="list-group">
-								{module.lessons?.map((lesson, index) => (
-									<li className="list-group-item" key={index}>
-										<FaEllipsisV className="me-2" />
-										{lesson.name}
-										<span className="float-end">
-											<FaCheckCircle className="text-success" />
+				{moduleList
+					.filter((module) => module.course === courseId)
+					.map((module, index) => (
+						<li key={index} className="list-group-item">
+							<div className="flex-fill">
+								<FaEllipsisV className="me-2" />
+								{module.name}
+								<span className="float-end">
+									<FaCheckCircle className="text-success" />
+									<FaPlusCircle
+										onClick={() => setSelectedModule(module)}
+										className="ms-2"
+									/>
+									<span className="dropdown">
+										<button
+											className="bg-transparent"
+											id="module-dropdown"
+											type="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false">
 											<FaEllipsisV className="ms-2" />
-										</span>
-									</li>
-								))}
-							</ul>
-						)}
-					</li>
-				))}
+										</button>
+										<ul
+											className="dropdown-menu"
+											aria-labelledby="module-dropdown">
+											<li>
+												<button
+													className="dropdown-item"
+													onClick={() => dispatch(deleteModule(module._id))}>
+													<FaRegTrashAlt className="text-danger" />
+												</button>
+												<button
+													className="dropdown-item"
+													onClick={(event) => {
+														event.preventDefault();
+														dispatch(setModule(module));
+														setShowModuleEdit(true);
+													}}>
+													<FaRegPenToSquare />
+												</button>
+											</li>
+										</ul>
+									</span>
+								</span>
+							</div>
+							{selectedModule._id === module._id && (
+								<ul className="list-group">
+									{module.lessons?.map((lesson: any, index: number) => (
+										<li className="list-group-item" key={index}>
+											<FaEllipsisV className="me-2" />
+											{lesson.name}
+											<span className="float-end">
+												<FaCheckCircle className="text-success" />
+												<FaEllipsisV />
+											</span>
+										</li>
+									))}
+								</ul>
+							)}
+						</li>
+					))}
 			</ul>
+
+			{showNewModule && (
+				<div className="bg-light border border-dark rounded p-3 position-fixed top-50 start-50 translate-middle">
+					<input
+						placeholder={"New Module Name"}
+						className="form-control"
+						onChange={(e) =>
+							dispatch(setModule({ ...module, name: e.target.value }))
+						}
+					/>
+					<textarea
+						placeholder={"Description"}
+						className="form-control"
+						onChange={(e) =>
+							dispatch(setModule({ ...module, description: e.target.value }))
+						}
+					/>
+					<button
+						className="btn btn-success"
+						onClick={() => {
+							dispatch(addModule({ ...module, course: courseId }));
+							setShowNewModule(false);
+						}}>
+						Add
+					</button>
+					<button
+						className="btn btn-danger"
+						onClick={() => setShowNewModule(false)}>
+						Cancel
+					</button>
+				</div>
+			)}
+			{showModuleEdit && (
+				<div className="w-50 bg-light border border-dark rounded p-3 position-fixed top-50 start-50 translate-middle">
+					<input
+						value={module.name}
+						className="form-control"
+						onChange={(e) =>
+							dispatch(setModule({ ...module, name: e.target.value }))
+						}
+					/>
+					<textarea
+						value={module.description}
+						className="form-control"
+						onChange={(e) =>
+							dispatch(setModule({ ...module, description: e.target.value }))
+						}
+					/>
+					<button
+						className="btn btn-success"
+						onClick={() => {
+							dispatch(updateModule(module));
+							setShowModuleEdit(false);
+						}}>
+						Update
+					</button>
+					<button
+						className="btn btn-danger"
+						onClick={() => setShowModuleEdit(false)}>
+						Cancel
+					</button>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,6 +7,8 @@ import Courses from "./Courses";
 import Dropdown from "./Courses/Navigation/Dropdown";
 import { courses } from "./Database";
 import { useState } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Kanbas() {
 	const [coursesState, setCourses] = useState(courses);
@@ -29,7 +31,6 @@ function Kanbas() {
 			...defaultCourse,
 			_id: new Date().getTime().toString(),
 		};
-		console.log(newCourse);
 		setCourses([...coursesState, newCourse]);
 		setShowNew(false);
 	};
@@ -51,48 +52,50 @@ function Kanbas() {
 		setShowEdit(false);
 	};
 	return (
-		<div className="d-flex">
-			<div>
-				<KanbasNavigation />
-			</div>
-			<div className={"d-flex flex-column flex-fill"}>
-				<Dropdown />
-				<div style={{ flexGrow: 1 }}>
-					<Routes>
-						<Route path="/" element={<Navigate to="Dashboard" />} />
-						<Route path="Account" element={<Account />} />
-						<Route
-							path="Dashboard"
-							element={
-								<Dashboard
-									coursesState={coursesState}
-									setCourses={setCourses}
-									defaultCourse={defaultCourse}
-									setDefaultCourse={setDefaultCourse}
-									showNew={showNew}
-									setShowNew={setShowNew}
-									showEdit={showEdit}
-									setShowEdit={setShowEdit}
-									addNewCourse={addNewCourse}
-									deleteCourse={deleteCourse}
-									updateCourse={updateCourse}
-								/>
-							}
-						/>
-						<Route
-							path="Courses/:courseId/*"
-							element={<Courses courses={coursesState} />}
-						/>
-						<Route path="Calendar/*" element={<h1>Calendar</h1>} />
-						<Route path="Inbox/*" element={<h1>Inbox</h1>} />
-						<Route path="History/*" element={<h1>History</h1>} />
-						<Route path="Studio/*" element={<h1>Studio</h1>} />
-						<Route path="Commons/*" element={<h1>Commons</h1>} />
-						<Route path="Help/*" element={<h1>Help</h1>} />
-					</Routes>
+		<Provider store={store}>
+			<div className="d-flex">
+				<div>
+					<KanbasNavigation />
+				</div>
+				<div className={"d-flex flex-column flex-fill"}>
+					<Dropdown />
+					<div style={{ flexGrow: 1 }}>
+						<Routes>
+							<Route path="/" element={<Navigate to="Dashboard" />} />
+							<Route path="Account" element={<Account />} />
+							<Route
+								path="Dashboard"
+								element={
+									<Dashboard
+										coursesState={coursesState}
+										setCourses={setCourses}
+										defaultCourse={defaultCourse}
+										setDefaultCourse={setDefaultCourse}
+										showNew={showNew}
+										setShowNew={setShowNew}
+										showEdit={showEdit}
+										setShowEdit={setShowEdit}
+										addNewCourse={addNewCourse}
+										deleteCourse={deleteCourse}
+										updateCourse={updateCourse}
+									/>
+								}
+							/>
+							<Route
+								path="Courses/:courseId/*"
+								element={<Courses courses={coursesState} />}
+							/>
+							<Route path="Calendar/*" element={<h1>Calendar</h1>} />
+							<Route path="Inbox/*" element={<h1>Inbox</h1>} />
+							<Route path="History/*" element={<h1>History</h1>} />
+							<Route path="Studio/*" element={<h1>Studio</h1>} />
+							<Route path="Commons/*" element={<h1>Commons</h1>} />
+							<Route path="Help/*" element={<h1>Help</h1>} />
+						</Routes>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Provider>
 	);
 }
 export default Kanbas;
